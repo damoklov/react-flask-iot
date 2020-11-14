a5265a8d5b64fd92864cf95cc4d82598093bc127
damoklov <mishanya@protonmail.com>
2020-11-15 01:54:50 +0200

    Updated Shop page

--- src/Shop.js
+++ src/Shop.js
@@ -1,6 +1,19 @@
 import './App.css';
 import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom';
+import { Button } from 'react-native';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
 function Shop() {
     useEffect(() => {
@@ -8,7 +21,7 @@ function Shop() {
         }, []);
 
     const [items, setItems] = useState([]);
-
+    const classes = useStyles();
     const fetchItems = async () => {
         const data = await fetch('http://127.0.0.1:5000/smart_home_appliance');
         const items = await data.json();
@@ -18,11 +31,25 @@ function Shop() {
 
     return (
         <div className="Shop">
+            <div className="shop__items">
             {items.map(item => (
+                <div className="shop__item">
                 <h1 key={item.id}>
                     <Link to={`/shop/${item.id}`}>{item._appliance_name}</Link>
                 </h1>
+                <img src={item._pic} alt={"Pic"} width={"200px"} height={"200px"} className={"center"}/>
+                    <div className={classes.root}>
+                        <h2>${item._repair_price}</h2>
+                        <IconButton color="primary" aria-label="add to shopping cart">
+                            <AddShoppingCartIcon />
+                        </IconButton>
+                        <IconButton color="primary" aria-label="info" key={item.id} href={`/shop/${item.id}`}>
+                            <InfoIcon />
+                        </IconButton>
+                    </div>
+                </div>
             ))}
+            </div>
         </div>
     )
 }
