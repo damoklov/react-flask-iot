bb864827771262e72e83d50d7267b851aedf68c1
damoklov <mishanya@protonmail.com>
2020-11-30 11:46:06 +0200

    Modified Shop to use Redux

--- src/Shop.js
+++ src/Shop.js
@@ -1,89 +1,133 @@
 import './App.css';
-import React, {Fragment, useState, useEffect} from 'react'
+import React, {Fragment, Component} from 'react'
 import {Link} from 'react-router-dom';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
 import axios from "axios";
 import useStyles from './Constants';
+import { connect } from 'react-redux';
+import { mapStateToProps, mapDispatchToProps} from "./components/actions/shopActions";
 
-function Shop() {
-    const [loading, setLoading] = useState(true);
-    const [search, setSearch] = useState(true);
-    const [filteredItems, setFilteredItems] = useState([]);
-    const [data, setData] = useState('');
-    const [query, setQuery] = useState('');
-    const classes = useStyles();
+class Shop extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+            search: true,
+            filteredItems: [],
+            data: '',
+            query: '',
+            cart: [],
+            classes: useStyles
+        };
+    }
 
-  useEffect(() => {
-      setSearch(false);
-    const fetchData = async () => {
-        if (search === true) {
-            setLoading(true);
-        if (query === '' && data === '') {
-            const result = await axios(`http://127.0.0.1:5000/smart_home_appliance`);
-            setFilteredItems(result.data.smart_home_appliances);
-        } else if (query !== '' && data === '') {
-            const result = await axios(`http://127.0.0.1:5000/smart_home_appliance?protocol=${query}`);
-            setFilteredItems(result.data.smart_home_appliances);
-        } else if (query === '' && data !== '') {
-            const result = await axios(`http://127.0.0.1:5000/smart_home_appliance/search/${data}`);
-            setFilteredItems(result.data.smart_home_appliances);
-        } else {
-            const result = await axios(`http://127.0.0.1:5000/smart_home_appliance/search/${data}?protocol=${query}`);
-            setFilteredItems(result.data.smart_home_appliances);
+    async componentDidMount() {
+        if (this.state.search === true) {
+            this.state.loading = true;
+            if (this.state.query === '' && this.state.data === '') {
+                const result = await axios(`http://127.0.0.1:5000/smart_home_appliance`);
+                this.state.filteredItems = result.data.smart_home_appliances;
+            } else if (this.state.query !== '' && this.state.data === '') {
+                const result = await axios(`http://127.0.0.1:5000/smart_home_appliance?protocol=${this.state.query}`);
+                this.state.filteredItems = result.data.smart_home_appliances;
+            } else if (this.state.query === '' && this.state.data !== '') {
+                const result = await axios(`http://127.0.0.1:5000/smart_home_appliance/search/${this.state.data}`);
+                this.state.filteredItems = result.data.smart_home_appliances;
+            } else {
+                const result = await axios(`http://127.0.0.1:5000/smart_home_appliance/search/${this.state.data}?protocol=${this.state.query}`);
+                this.state.filteredItems = result.data.smart_home_appliances;
+            }
+            this.state.loading = false;
+            this.state.data = '';
+            this.state.query = '';
+            this.state.search = false;
+            this.setState(this.state);
+            console.log(this.state);
         }
-        setLoading(false);
-        setQuery('');
-        setData('');}
     };
-    fetchData();
-  }, [search, data, query]);
 
-    return (
-    <Fragment>
-        {loading ? (
-            <div className="Shop">
-                 <div className="loader">Loading...</div>
-             </div>
-      ) : (
-        <div className="Shop">
-          <input
-            type="text"
-            placeholder="Protocol"
-            onChange={(e) => setQuery(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Item Title"
-            onChange={(e) => setData(e.target.value)}
-          />
-          <button type="button" onClick={() => setSearch(true)}>
-              Search
-          </button>
-        <div className="shop__items">
-        {filteredItems.map(item => (
-            <div className="shop__item">
-            <h1 key={item.id}>
-                <Link to={`/shop/${item.id}`}>{item._appliance_name}</Link>
-            </h1>
-            <img src={item._pic} alt={"Pic"} width={"200px"} height={"200px"} className={"center"}/>
-                <div className={classes.root}>
-                    <h2>${item._repair_price}</h2>
-                    <IconButton color="primary" aria-label="add to shopping cart">
-                        <AddShoppingCartIcon />
-                    </IconButton>
-                    <IconButton color="primary" aria-label="info" key={item.id} href={`/shop/${item.id}`}>
-                        <InfoIcon />
-                    </IconButton>
-                </div>
-            </div>
-        ))}
-        </div>
-        </div>
-    )}
-    </Fragment>
-    );
+    async componentDidUpdate(previousProps, previousState) {
+        if (this.state.search === true) {
+            this.state.loading = true;
+            if (this.state.query === '' && this.state.data === '') {
+                const result = await axios(`http://127.0.0.1:5000/smart_home_appliance`);
+                this.state.filteredItems = result.data.smart_home_appliances;
+            } else if (this.state.query !== '' && this.state.data === '') {
+                const result = await axios(`http://127.0.0.1:5000/smart_home_appliance?protocol=${this.state.query}`);
+                this.state.filteredItems = result.data.smart_home_appliances;
+            } else if (this.state.query === '' && this.state.data !== '') {
+                const result = await axios(`http://127.0.0.1:5000/smart_home_appliance/search/${this.state.data}`);
+                this.state.filteredItems = result.data.smart_home_appliances;
+            } else {
+                const result = await axios(`http://127.0.0.1:5000/smart_home_appliance/search/${this.state.data}?protocol=${this.state.query}`);
+                this.state.filteredItems = result.data.smart_home_appliances;
+            }
+            this.state.loading = false;
+            this.state.data = '';
+            this.state.query = '';
+            this.state.search = false;
+            this.setState(this.state);
+            console.log(this.state);
+        }
+    }
+
+    render() {
+        return (
+            <Fragment>
+                {this.state.loading ? (
+                    <div className="Shop">
+                        <div className="loader">Loading...</div>
+                    </div>
+                ) : (
+                    <div className="Shop">
+                        <input
+                            type="text"
+                            placeholder="Protocol"
+                            onChange={(e) => this.setState({query: e.target.value})}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Item Title"
+                            onChange={(e) => this.setState({data: e.target.value})}
+                        />
+                        <button type="button" onClick={() => this.setState({loading: true, search: true})}>
+                            Search
+                        </button>
+                        <div className="shop__items">
+                            {this.state.filteredItems.map((item, index) => (
+                                <div className="shop__item">
+                                    <h1 key={index}>
+                                        <Link
+                                            to={`/shop/${item.id}`}>{item._appliance_name}</Link>
+                                    </h1>
+                                    <img src={item._pic} alt={"Pic"}
+                                         width={"200px"} height={"200px"}
+                                         className={"center"}/>
+                                    <div className={this.state.classes.root}>
+                                        <h2>${item._repair_price}</h2>
+                                        <IconButton
+                                            onClick={() => this.props.addToCart(item)}
+                                            color="primary"
+                                            aria-label="add to shopping cart">
+                                            <AddShoppingCartIcon/>
+                                        </IconButton>
+                                        <IconButton color="primary"
+                                                    aria-label="info"
+                                                    key={index}
+                                                    href={`/shop/${item.id}`}>
+                                            <InfoIcon/>
+                                        </IconButton>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                )}
+            </Fragment>
+        );
+    }
 }
 
-export default Shop;
+export default connect(mapStateToProps, mapDispatchToProps)(Shop);
