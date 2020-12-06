01ecc9766bb5117655ba274564c327c45d5f8415
damoklov <mishanya@protonmail.com>
2020-12-06 15:46:50 +0200

    Deleted rebased files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import Cart from './Cart';
-import Shop from './Shop';
-import Footer from './Footer';
-import Nav from './Nav';
-import Home from './Home';
-import ItemDetail from './ItemDetails';
+import Cart from './components/Cart/Cart';
+import Shop from './components/Shop/Shop';
+import Footer from './components/Home/Footer';
+import Nav from './components/Navigation/Nav';
+import Home from './components/Home/Home';
+import ItemDetail from './components/Shop/ItemDetails';
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Checkout from "./components/CheckoutPage/Checkout";
 
 class App extends Component {
     render() {
@@ -19,6 +20,7 @@ class App extends Component {
                 <Route path={"/cart"} exact component={Cart}/>
                 <Route path={"/shop"} exact component={Shop}/>
                 <Route path={"/shop/:id"} component={ItemDetail} />
+                <Route path={"/checkout"} exact component={Checkout} />
                 </Switch>
             </Router>
             <Footer/>
